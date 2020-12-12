06494f8f614b225002ec82550028990cfcda42ac
Rahul Ramteke <rahul24ramteke@gmail.com>
2020-12-12 13:29:03 +0530

    create header, note and footer components and style them.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App";
 
 
 ReactDOM.render(
-  <h1>Hello World! Good Morning.</h1>,
+  <App />,
   document.getElementById('root')
 );
 
